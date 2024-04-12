6a89f0d08e2440caa48a3861da52b8c114ac60a7
minseonnnn <mm1687d@naver.com>
2024-04-12 17:46:56 +0900

    0412 자바연습

---
+++ Chapter02_변수와연산자_25/src/변수_7.java
@@ -0,0 +1,21 @@
+/*
+ *   값을 교환 
+ */
+public class 변수_7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int a=100;
+        int b=200;
+        int temp=a; // 임시 저장
+        a=b; // a=200
+        b=temp; // b=a ==> b=200
+        System.out.println("a="+a);
+        System.out.println("b="+b);
+        
+        char c1='A';
+        char c2='B';
+        // 정렬
+	}
+
+}
