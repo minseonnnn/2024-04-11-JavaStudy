baa130a0a3cac36b4963f21b340a7ae5b472ecf6
sistt110 <sistt110@SIST110>
2024-04-12 15:56:51 +0900

    0412

---
+++ Chapter02_변수와연산자_25/src/변수_4.java
@@ -0,0 +1,23 @@
+/*
+ *   돈 : 5600원 
+ *        => 지출
+ *           점심 : 3000원
+ *           커피 : 1500원
+ *           심심풀이 : 250원
+ *           
+ */
+
+
+public class 변수_4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int A=3000;
+        int B=1500;
+        int C=250;
+        int D=5600;
+    
+        System.out.println(D-A-B-C+"원");
+	}
+
+}
