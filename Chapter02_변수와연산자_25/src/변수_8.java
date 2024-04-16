ebde590b67c68aad2f75b71378d8a9db4ae13cee
minseonnnn <mm1687d@naver.com>
2024-04-16 14:27:53 +0900

    0416 자바연습

--- Chapter02_변수와연산자_25/src/변수_8.java
+++ Chapter02_변수와연산자_25/src/변수_8.java
@@ -1,64 +1,63 @@
 /*
- *      30 20 40 50 10
- *      --------------   10 20 30 40 50
- *      -- --
- *      20 30 
- *      --    --
- *      20    40
- *      --       --
- *      20       50
- *      --          --
- *      10          20
- *      -----------------
- *      10 30  40 50 20
- *         --  --
- *         30  40
- *         --     --
- *         30     50
- *         --        --
- *         20        30
- *      ------------------
- *      10 20  40 50 30
- *             -- --
- *             40 50
- *             --    --
- *             30    40
- *      -------------------
- *      10 20 30  50 40
- *                -- --
- *                40 50
- *      --------------------
- *      10 20 30 40 50
- *      --------------------------- Select Sort                 
- *                   
+ *     30 20 40 50 10
+ *     --------------   10 20 30 40 50 
+ *     -- --
+ *     20 30
+ *     --    --
+ *     20    40
+ *     --       --
+ *     20       50
+ *     --          --
+ *     10          20
+ *     -------------------
+ *     10 30  40 50 20
+ *        --  --
+ *        30  40
+ *        --     --
+ *        30     50
+ *        --        --
+ *        20        30
+ *     --------------------
+ *     10 20  40 50 30
+ *            -- --
+ *            40 50
+ *            --    --
+ *            30    40
+ *     --------------------
+ *     10 20 30  50 40
+ *               -- --
+ *               40  50
+ *     --------------------
+ *     10 20 30 40 50
+ *     -------------------------- Select Sort
  */
 
-import java.lang.reflect.Array;
+import java.util.Arrays;
 
 public class 변수_8 {
 
 	public static void main(String[] args) {
-	    	// TODO Auto-generated method stub
-            int[] arr= {30 ,20, 40, 50, 10};
-            System.out.println("변경전:");
-            System.out.println(Array.toString(arr));
-            Array.sort(arr);
-            /*
-            for(int i=0;i<arr.length-1;i++)
-            {
-            	for(int j=i+1;j<arr.length;j++)
-        	    {
-        		    if(arr[i]>arr[j])
-        		     {
-        		      int temp=arr[i];
-             	 	 arr[i]=arr[j];
-        	     	 arr[j]=temp;
-        	    	}
-        	    }
-            }*/
-            System.out.println("변경후:");
-            System.out.println(Arrays.toString(arr));
+		// TODO Auto-generated method stub
+        int[] arr= {30 ,20, 40, 50, 10};
+        System.out.println("변경전:");
+        System.out.println(Arrays.toString(arr));
+        Arrays.sort(arr);
+        /*
+        for(int i=0;i<arr.length-1;i++)
+        {
+        	for(int j=i+1;j<arr.length;j++)
+        	{
+        		if(arr[i]>arr[j])
+        		{
+        		 int temp=arr[i];
+        		 arr[i]=arr[j];
+        		 arr[j]=temp;
+        		}
+        	}
+        }*/
+        System.out.println("변경후:");
+        System.out.println(Arrays.toString(arr));
         
-	    }
+	}
 
-    }
+}
