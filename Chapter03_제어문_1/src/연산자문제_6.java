c1a86d23d1ead516c96b9f25ac1fb6b065e8b22b
sistt110 <sistt110@SIST110>
2024-04-16 17:57:05 +0900

    0416

---
+++ Chapter03_제어문_1/src/연산자문제_6.java
@@ -0,0 +1,27 @@
+/*
+ *      int num1=10, num2=10;   
+        int a, b;   
+        a = ++num1;
+            --
+            => 전치 연산자 => 먼저 증가
+          -- 증가된 값을 대입
+        a=11 num1=11    
+        b = num2++;
+        --------
+          1 => 후치연산자 => 대입
+            ------
+              2 => 증가
+              
+        System.out.printf("%d, %d \n", b, num2);
+                                          =====  11
+                                      === 10
+                                           
+ */
+public class 연산자문제_6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
