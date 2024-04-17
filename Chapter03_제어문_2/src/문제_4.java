e8464d158e8ef54cd8ef1054f3b141ad8dd74686
sistt110 <sistt110@SIST110>
2024-04-17 17:53:46 +0900

    0417

---
+++ Chapter03_제어문_2/src/문제_4.java
@@ -0,0 +1,38 @@
+/*
+ *  4) 년도를 입력 받아 윤년인지 아닌지를 판별하는 방법을 if~else문으로 사용하라
+ * 
+ *  윤년 조건 => 로마시대 => 윤년 계산법 , 암호화 ...
+ *  4년마다
+ *  100년마다 제외
+ *  400년마다
+ *  --------------
+ *  if((year%4==0 && year%100!=0)||(year%400==0))
+ *		{
+ *		}
+ *		else
+ *		{
+ *		}
+ *  
+ */
+import java.util.Scanner;
+public class 문제_4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan=new Scanner(System.in);
+		System.out.print("년도 입력:");
+		int year=scan.nextInt();
+		// 처리 ==> 윤년 조건
+		if((year%4==0 && year%100!=0)||(year%400==0))
+		{
+			System.out.println(year+"년도는 윤년입니다");
+		}
+		else
+		{
+			System.out.println(year+"년도는 윤년이 아닙니다");
+		}
+		
+
+	}
+
+}
---
+++ Chapter03_제어문_2/src/문제_7.java
@@ -0,0 +1,29 @@
+/*
+ *    7) 정수 2개와 연산자(+,-,*,/)를 입력 받아 사칙 연산하는 프로그램을 만들어라 
+         (다중조건문 사용)
+ */
+public class 문제_7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int num1=(int)(Math.random()*100)+1;
+        int num2=(int)(Math.random()*100)+1;
+        int ops=(int)(Math.random()*4); // 0~3
+        //            0.0~0.99 => 0~3
+        char op=' ';
+        if(ops==0) op='+';
+        else if(ops==1) op='-';
+        else if(ops==2) op='*';
+        else if(ops==2) op='/';
+        
+        int num3=0;
+        if(op=='+') num3=num1+num2;
+        else if(op=='-') num3=num1-num2;
+        else if(op=='*') num3=num1*num2;
+        else if(op=='/') num3=num1/num2;
+        
+        System.out.printf("%d %c %d = %d",num1,op,num2,num3);
+        // 프로그램 => 개발(X) , 유지보수 (재사용) => 수정/추가
+	}
+
+}
