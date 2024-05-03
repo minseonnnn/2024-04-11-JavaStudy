426e51c238026b520a240cc1c93a5300e6b0f69c
minseonnnn <mm1687d@naver.com>
2024-05-03 17:51:54 +0900

    0503

---
+++ Chapter05_클래스_1/src/문제_1.java
@@ -0,0 +1,12 @@
+class music
+{
+	int rank;
+}
+public class 문제_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Chapter05_클래스_1/src/클래스_3.java
@@ -0,0 +1,48 @@
+/*
+ *   int a=1,b=3,c=2,d=4,e=6,f=5,f=12000;
+ *   int max=a;
+ *   if(max<b)
+ *      max=b;
+ *   if(max<c)
+ *      max=c;
+ *   if(max<d)
+ *      max=d;
+ *   if(max<e)
+ *      max=e;
+ *   if(max<f)
+ *      max=f;            
+ *   int[] arr=new int[6]
+ *   int max=arr[0]
+ *   for(int i:arr)
+ *   {
+ *      if(max<i)
+ *        max=i
+ *   }
+ *   
+ *   String[] name
+ *   int[] kor
+ *   int[] math
+ *   int[] eng
+ *   int[] total
+ *   double[] avg
+ *   int[] rank
+ *   char[] score
+ *   ------------------- 사용자 정의 데이터형 
+ *   class Student
+ *   {
+ *      String name;
+ *      int kor,eng,math,total,rank;
+ *      char score;
+ *      double avg;
+ *   }
+ *   
+ *   Student[] std=new Student[10]
+ */
+public class 클래스_3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
