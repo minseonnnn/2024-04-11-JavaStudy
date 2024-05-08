4a5f03323fb48c985c879f2b19375662804d7498
minseonnnn <mm1687d@naver.com>
2024-05-08 09:22:55 +0900

    0503

--- Chapter05_클래스_1/src/문제_1.java
+++ Chapter05_클래스_1/src/문제_1.java
@@ -1,6 +1,13 @@
-class music
+class Music
 {
 	int rank;
+	String title;
+	String poster;
+	String singer;
+	String album;
+	String state;//유지 상승 하강
+	int idcrement;// 등폭 
+	int like;//좋아요 
 }
 public class 문제_1 {
 
