90c04a045d2fba2b4dd03131a979a3fac05d793c
sistt110 <sistt110@SIST110>
2024-05-27 17:51:32 +0900

    0527

---
+++ Chapter10_라이브러리(java.util)_4/src/com/sist/map/JoinModel.java
@@ -0,0 +1,9 @@
+package com.sist.map;
+
+public class JoinModel implements Model{
+	 public void execute()
+	   {
+		   System.out.println("회원가입 관련 요청...");
+	   }
+
+}
---
+++ Chapter11_라이브러리(java.io)_1/src/com/sist/io/라이브러리_9.java
@@ -0,0 +1,30 @@
+package com.sist.io;
+// 파일쓰기 => 문자 스트림(2byte) => 한글 사용이 편리
+// FileWriter
+import java.io.*;
+public class 라이브러리_9 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        FileWriter fw=null;
+        try
+        {
+        	fw=new FileWriter("c:\\java_data\\board.txt",true); // 없는 경우에는 자동 생성
+        	// 폴더는 생성하지 않는다 
+        	String msg="ABCDEFGHIJKLMNOPOOOOOOOOOOOOOOO123456789\r\n";
+        	fw.write(msg);
+        	System.out.println("저장완료!!");
+        }catch(Exception ex)
+        {
+        	ex.printStackTrace();
+        }
+        finally
+        {
+        	try
+        	{
+        		fw.close();
+        	}catch(Exception ex) {}
+        }
+	}
+
+}
