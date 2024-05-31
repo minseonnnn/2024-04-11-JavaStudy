3b917c2dc2abab506e65b9ff7e74ffed61cb4d90
minseonnnn <mm1687d@naver.com>
2024-05-31 17:44:54 +0900

    0531 자바연습

---
+++ JavaApplicationProject_2/src/com/sist/temp/ZipcodeVO.java
@@ -0,0 +1,11 @@
+package com.sist.temp;
+import java.util.*;
+import lombok.Data;
+@Data
+public class ZipcodeVO {
+		   private String zipcode;
+		   private String sido;
+		   private String gugun;
+		   private String dong;
+		   private String bunji;
+}
