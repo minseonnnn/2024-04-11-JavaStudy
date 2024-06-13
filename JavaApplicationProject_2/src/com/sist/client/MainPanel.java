bccd1316bd79fb8fcd1ff0b88f81c93db62a7e22
sistt110 <sistt110@SIST110>
2024-06-13 09:00:07 +0900

    0613

--- JavaApplicationProject_2/src/com/sist/client/MainPanel.java
+++ JavaApplicationProject_2/src/com/sist/client/MainPanel.java
@@ -2,15 +2,13 @@ package com.sist.client;
 import java.awt.*;
 import javax.swing.*;
 public class MainPanel extends JPanel{
-     MenuPanel menuP=new MenuPanel();
-     ControllPanel ctrP=new ControllPanel();
-     
+     MenuPanel mp=new MenuPanel();
+     ControllPanel cp=new ControllPanel();
      //EmpListPanel ep=new EmpListPanel();
      public MainPanel()
      {
     	 setLayout(new BorderLayout());
-    	 add("MENU", menuP);
-    	 add("Center",ctrP);
-    	 
+    	 add("North",mp);
+    	 add("Center",cp);
      }
 }
