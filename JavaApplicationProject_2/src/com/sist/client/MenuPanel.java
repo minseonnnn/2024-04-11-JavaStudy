a74248f2deb1414453f0a360fbf535c6ba218b6e
minseonnnn <mm1687d@naver.com>
2024-06-18 15:12:01 +0900

    0618

--- JavaApplicationProject_2/src/com/sist/client/MenuPanel.java
+++ JavaApplicationProject_2/src/com/sist/client/MenuPanel.java
@@ -2,16 +2,17 @@ package com.sist.client;
 import java.awt.*;
 import javax.swing.*;
 public class MenuPanel extends JPanel{
-    JButton homeBtn,findBtn,boardBtn,chatBtn,newsBtn,exitBtn;
+    JButton homeBtn,findBtn,boardBtn,chatBtn,newsBtn,myBtn,exitBtn;
     public MenuPanel()
     {
     	setLayout(new GridLayout(1,6,5,5));
     	//setLayout(new GridLayout(6,1,5,5));
     	homeBtn=new JButton("홈"); // 1명
-    	findBtn=new JButton("맛집검색"); // 1명
+    	findBtn=new JButton("상품검색"); // 1명
     	boardBtn=new JButton("커뮤니티"); // 1명
     	chatBtn=new JButton("실시간 채팅"); // 2명
     	newsBtn=new JButton("뉴스 검색"); // 1명
+    	myBtn=new JButton("마이페이지");
     	exitBtn=new JButton("나가기");
     	
     	add(homeBtn);
@@ -19,6 +20,7 @@ public class MenuPanel extends JPanel{
     	add(boardBtn);
     	add(chatBtn);
     	add(newsBtn);
+    	add(myBtn);
     	add(exitBtn);
     	
     	
